022f750f74fedca57bc55d3c65e0056c208609b5
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-07-16 13:38:10 +0200

    nuyoeeiukl

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,6 +23,8 @@
         </style>
     </head>
     <body>
-    <h1 class="text-3xl font-bold underline">      Hello world!    </h1>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
     </body>
 </html>
