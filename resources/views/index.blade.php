93c945a7f3fdf4fcaa184aa7b647167a34142911
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-07-16 18:11:50 +0200

    interm

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,12 +33,27 @@
                         </a>
                         <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full">
                             <div class="font-semibold text-xs  flex justify-center items-center h-full" style="right:-20px;bottom: -20px;">
-                                80%</div>
+                                80%
+                            </div>
 
 
                         </div>
 
                     </div>
+                    <div class="ml-12">
+                        <a href="#" class="block text-lg font-semibold leading-tight
+                        hover:text-gray-400 mt-4">Final fantasy </a>
+                        <div class="text-gray-400 mt-1">Playstation 4
+                            <p class="mt-6 text-gray-400">
+                            lewalibari
+                            </p>
+
+                        </div>
+
+
+
+                    </div>
+
                     <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 ml-8">
                         final fantasy
                     </a>
@@ -94,11 +109,25 @@
          kkkkkk lllopppp pooo
          kkkkk
      </div>
-     <div class="most-ancipated w-1/4 mr-32">
+     <div class="most-ancipated w-1/4 ">
+         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most anticpatired</h2>
+          <div class="most-anticipated-container space-y-10 mt-8">
+               <div class="game flex">
+                   <a href="#">
+                       <img src="" alt="games over "   class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                   </a>
+                   <div class="ml-4">
+                       <a href="" class="hover:text-gray-300">Cyberpank 277</a>
+
+                       <div class="text-gray-400 text-sm mt-1">Sept 16 ,300
+                           <div>
+                   </div>
+                   </div>
+
+          </div>
 
          kkllmm ojjjj
      </div>
-
 
  </div>
 @endsection
