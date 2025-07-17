90eaabce5908c9f927f09a5e81546249391f7dfe
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-07-18 01:54:08 +0200

    ererpppplmopiu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,6 +47,32 @@
                 </div>
 
 
+                <div class="game ml-8">
+                    <div class="relative inline-block">
+                        <a href="#">
+                            <img src="/img/Capture d’écran 2025-05-28 à 02.14.45.png" alt="games over "   class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full">
+                            <div class="font-semibold text-xs  flex justify-center items-center h-full" style="right:-20px;bottom: -20px;">
+                                80%
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
+
+                    <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 ml-8">
+
+                        final fantasy
+                    </a>
+                    <div class="text-gray-400 mt-1">Playstation 4 </div>
+
+
+                </div>
+
+
                 <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 ml-8">
 
                     final fantasy
