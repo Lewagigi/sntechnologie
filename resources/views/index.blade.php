dc6eae300288c59e538830592ac2e979372a4d1e
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-07-18 02:11:26 +0200

    ererpppplmopiuoiuop

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,95 +5,18 @@
     <div class="container mx-auto px-4">
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Gray</h2>
         <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6  gap-12 border-b border-gray-800 pb-16">
-            <div class="game ml-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/img/Capture d’écran 2025-05-28 à 02.14.45.png" alt="games over "   class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full">
-                        <div class="font-semibold text-xs  flex justify-center items-center h-full" style="right:-20px;bottom: -20px;">
-                            80%
-                        </div>
-
-
-                    </div>
-
-                </div>
-
-
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 ml-8">
-
-                    final fantasy
-                </a>
-                <div class="text-gray-400 mt-1">Playstation 4 </div>
-
-
-            </div>
-
-
-            <div class="game ml-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/img/Capture d’écran 2025-05-28 à 02.14.45.png" alt="games over "   class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full">
-                        <div class="font-semibold text-xs  flex justify-center items-center h-full" style="right:-20px;bottom: -20px;">
-                            80%
-                        </div>
-
-
-                    </div>
-
-                </div>
 
 
 
 
 
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 ml-8">
-                    <div class="game ml-8">
-                        <div class="relative inline-block">
-                            <a href="#">
-                                <img src="/img/Capture d’écran 2025-05-28 à 02.14.45.png" alt="games over "   class="hover:opacity-75 transition ease-in-out duration-150">
-                            </a>
-                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full">
-                                <div class="font-semibold text-xs  flex justify-center items-center h-full" style="right:-20px;bottom: -20px;">
-                                    80%
-                                </div>
 
 
-                            </div>
-
-                        </div>
 
 
-                        <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 ml-8">
-
-                            final fantasy
-                        </a>
-                        <div class="text-gray-400 mt-1">Playstation 4 </div>
 
 
-                    </div>
-
-                    final fantasy
-                </a>
-                <div class="text-gray-400 mt-1">Playstation 4 </div>
-
-
-            </div>
-
-
-            <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
-                <div class="game ml-8">
-                    <div class="relative inline-block">
-                        <a href="#">
-                            <img src="/img/Capture d’écran 2025-05-28 à 02.14.45.png" alt="games over "   class="hover:opacity-75 transition ease-in-out duration-150">
-                        </a>
-                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full">
-                            <div class="font-semibold text-xs  flex justify-center items-center h-full" style="right:-20px;bottom: -20px;">
-                                80%
-                            </div>
+            
 
 
                         </div>
