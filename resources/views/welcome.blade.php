82ef6a7ceb51806013b5bfa071582dbf54f37007
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2025-07-18 22:01:39 +0200

    erteyruriop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+  
 
     <!-- Fonts -->
-   
+
 
     <!-- Styles / Scripts -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
